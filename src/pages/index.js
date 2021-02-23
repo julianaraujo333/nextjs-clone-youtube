bbf8abdac091d3eb8ca4f90d87e2aba6b48d4fa2
Juliana Araújo <julianaraujo555@outlook.com>
2021-02-23 15:02:31 -0300

    Layout components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import { Button } from '@material-ui/core';
+
+import Layout from './../components/Layout';
 export default function Home() {
   return (
-    <div>
+    <Layout title="Youtube">
       <Button variant="outlined" color="secondary">
         Secondary
       </Button>
-    </div>
+    </Layout>
   )
 }
