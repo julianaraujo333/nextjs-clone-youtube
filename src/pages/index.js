dc4d91cd21d7bb2ed7ab2c03137ba7eca8fe0cf6
Juliana Araújo <julianaraujo555@outlook.com>
2021-02-23 01:31:20 -0300

    configurando material-ui

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import { Button } from '@material-ui/core';
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outlined" color="secondary">
+        Secondary
+      </Button>
+    </div>
+  )
+}
